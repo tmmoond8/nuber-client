61c78ec1e8d0d0a98193ecc420589ffeec8dd289
moonti <tmmoond8@gmail.com>
2019-05-02 21:05:46 +0900

    #2.7 Typescript and Styled Components part One #2.8 Typescript and Styled Components part Two

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import { injectGlobal } from 'styled-components';
+import reset from "styled-reset";
+import theme from '../../theme';
+import { ThemeProvider } from '../../typed-components';
 import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from "./AppQueries";
 
+// tslint:disable-next-line
+injectGlobal`
+  ${reset}
+`;
+
 const AppContainer = ({ data }) => (
-  <AppPresenter isLoggedIn={data.auth.isLoggedIn}/>
+  <ThemeProvider theme={theme}>
+    <AppPresenter isLoggedIn={data.auth.isLoggedIn}/>
+  </ThemeProvider>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
