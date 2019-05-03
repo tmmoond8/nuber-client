35abceac1b5eb114ae500933bd3b503e5410b98e
moonti <tmmoond8@gmail.com>
2019-05-03 10:08:42 +0900

    #2.9 Global Styles Set Up

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ApolloProvider } from "react-apollo"
 import ReactDOM from 'react-dom';
 import apolloClient from "./apollo";
+import "./global-styles";
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
