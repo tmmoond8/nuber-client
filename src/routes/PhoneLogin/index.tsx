dd85090a3484e57524943db45a2d8ec5f880dea2
moonti <tmmoond8@gmail.com>
2019-05-03 18:19:48 +0900

    #2.14 Route Components Review

--- src/routes/PhoneLogin/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const R = ()  => <span>PhoneLogin</span>;
-
-export default R;
--- src/routes/SocialLogin/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const R = ()  => <span>SocialLogin</span>;
-
-export default R;
