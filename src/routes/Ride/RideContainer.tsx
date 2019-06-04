1d8694194e335f179cdf65507d657fa898f63248
moonti <tmmoond8@gmail.com>
2019-06-04 18:22:50 +0900

    #2.73 Get Ride Query part One

--- src/routes/Ride/RideContainer.tsx
+++ src/routes/Ride/RideContainer.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import RidePrsenter from './RidePresenter';
+import { Query } from 'react-apollo';
+import { RouteComponentProps } from 'react-router-dom';
+import { getRide, getRideVariables } from '../../types/api';
+import { GET_RIDE } from './Ride.queries';
+import RidePresenter from './RidePresenter';
 
-class RideContainer extends React.Component {
+class RideQuery extends Query<getRide, getRideVariables> {}
+
+interface IProps extends RouteComponentProps<any> {}
+
+class RideContainer extends React.Component<IProps> {
+  constructor(props) {
+    super(props);
+    const {
+      match: {
+        params: { rideId }
+      },
+      history
+    } = this.props;
+    if(!rideId || !parseInt(rideId, 10)) {
+      history.push('/')
+    }
+  }
   public render() {
-    return <RidePrsenter/>;
+    const {
+      match: {
+        params: { rideId }
+      },
+    } = this.props;
+    
+    return (
+      <RideQuery query={GET_RIDE} variables={{ rideId: parseInt(rideId, 10) }}>
+        {({ data: rideData }) => (
+          <RidePresenter rideData={rideData}/>
+        )}
+      </RideQuery>
+    )
   }
 }
 
--- src/routes/Ride/RidePresenter.tsx
+++ src/routes/Ride/RidePresenter.tsx
@@ -1,8 +1,91 @@
+import Button from 'components/Button';
 import React from 'react';
 import styled from '../../typed-components';
+import { getRide } from '../../types/api';
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 40px;
+`;
 
-const RidePresenter: React.SFC = () => <Container>Chat</Container>
+const Title = styled.h4`
+  font-weight: 800;
+  margin-top: 30px;
+  margin-bottom: 10px;
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+const Data = styled.span`
+  color: ${props => props.theme.blueColor};
+`;
+
+const Img = styled.img`
+  border-radius: 50%;
+  margin-right: 20px;
+  max-width: 50px;
+  height: 50px;
+`;
+
+const Passenger = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Buttons = styled.div`
+  margin: 30px 0px;
+`;
+
+const ExtendedButton = styled(Button)`
+  margin-bottom: 30px;
+`;
+
+interface IProps {
+  rideData?: getRide;
+}
+
+const RidePresenter: React.SFC<IProps> = ({
+  rideData: { GetRide: { ride = null } = {} } = { GetRide: { ride: null}},
+}) => (
+  <Container>
+    {ride && (
+        <React.Fragment>
+          <Title>Passenger</Title>
+          <Passenger>
+            <Img src={ride.passenger.profilePhoto!} />
+            <Data>{ride.passenger.fullName!}</Data>
+          </Passenger>
+          {ride.driver && (
+            <React.Fragment>
+              <Title>Driver</Title>
+              <Passenger>
+                <Img src={ride.driver.profilePhoto!} />
+                <Data>{ride.driver.fullName!}</Data>
+              </Passenger>
+            </React.Fragment>
+          )}
+          <Title>From</Title>
+          <Data>{ride.pickUpAddress}</Data>
+          <Title>To</Title>
+          <Data>{ride.dropOffAddress}</Data>
+          <Title>Price</Title>
+          <Data>{ride.price}</Data>
+          <Title>Distance</Title>
+          <Data>{ride.distance}</Data>
+          <Title>Duration</Title>
+          <Data>{ride.duration}</Data>
+          <Title>Status</Title>
+          <Data>{ride.status}</Data>
+          <Buttons>
+            <ExtendedButton
+              value={"Picked Up"}
+              onClick={() =>""}
+            />
+          </Buttons>
+        </React.Fragment>
+      )}
+  </Container>
+);
 
 export default RidePresenter;
